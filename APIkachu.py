18483d850a676c661892cbfc8a26bcb47ebf58b7
ConnorFieldUser <conquest.field@gmail.com>
2016-10-24 16:51:49 -0400

    why do I have so much trouble with basics

--- APIkachu.py
+++ APIkachu.py
@@ -1,13 +1,72 @@
 import requests
+#
+#
+# # LIST
+# def get_list_data(endpoint, lookup="name"):
+#     url = "http://pokeapi.co/api/v2/{}/".format(endpoint)
+#     print(url)
+#     while url:
+#         result = requests.get(url)
+#         json_result = result.json()
+#         for pokemon in json_result["results"]:
+#             print(pokemon[lookup])
+#
+#         if input("Press enter to keep going, subit anything else to quit"):
+#             break
+#
+#         url = json_result["next"]
+#
+# # while True:
+# #     value = input("Search: pokemon, generation, item: ").lower()
+# #     get_list_data(value)
+#
+#
+# # POKEMON
+# def get_detail_data(endpoint, lookup="name"):
+#     url = "http://pokeapi.co/api/v2/pokemon/{}/".format(endpoint)
+#     print(url)
+#     while url:
+#         result = requests.get(url)
+#         json_result = result.json()
+#         print(json_result['name'])
+#
+#         if input("Press enter to keep going, "):
+#             break
+#
+#         url = json_result["next"]
+#
+# # while True:
+# #     value = input("Search: ")
+# #     get_pokemon_detail_data(value)
+#
+#
+# # ITEM
+# def get_item_detail_data(endpoint, lookup="name"):
+#     url = "http://pokeapi.co/api/v2/item/{}/".format(endpoint)
+#     print(url)
+#     while url:
+#         result = requests.get(url)
+#         json_result = result.json()
+#         print(json_result['name'])
+#
+#         if input("Press enter to keep going, "):
+#             break
+#
+#         url = json_result["next"]
+#
+# # while True:
+# #     value = input("Search: ")
+# #     get_item_detail_data(value)
 
 
-def get_data(endpoint, lookup="name"):
-    url = "http://pokeapi.co/api/v2/pokemon/{}/".format(endpoint)
+# Generation
+def get_version_detail_data(endpoint, lookup="name"):
+    url = "http://pokeapi.co/api/v2/generation/{}/".format(endpoint)
+    print(url)
     while url:
         result = requests.get(url)
         json_result = result.json()
-        for pokemon in json_result["results"]:
-            print(pokemon[lookup])
+        print(json_result['name', 'version'])
 
         if input("Press enter to keep going, "):
             break
@@ -16,4 +75,8 @@ def get_data(endpoint, lookup="name"):
 
 while True:
     value = input("Search: ")
-    get_data(value)
+    get_version_detail_data(value)
+
+#
+# def user_input(self):
+#     what_do_you_want = input("Browse the full (LIST)s, or seach for a (POKEMON) an, (ITEM), or a (GENERATION)").lower
