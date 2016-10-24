59465f5f8c552fb0954efdb2de37b3b299a0bffd
ConnorFieldUser <conquest.field@gmail.com>
2016-10-24 18:58:06 -0400

    might be on to somehting

--- APIkachu.py
+++ APIkachu.py
@@ -4,7 +4,7 @@ import requests
 # LIST
 def get_list_data(endpoint, lookup="name"):
     url = "http://pokeapi.co/api/v2/{}/".format(endpoint)
-    print(url)
+    print("URL: {}".format(url))
     while url:
         result = requests.get(url)
         json_result = result.json()
@@ -30,10 +30,10 @@ def get_pokemon_detail_data(endpoint, lookup="name"):
         json_result = result.json()
         print(json_result['name'])
 
-        while not input("Press enter to search again, or type something to cancel "):
+        if not input("Press enter to search again, or type something to cancel "):
             url = "http://pokeapi.co/api/v2/pokemon/{}/".format(endpoint)
         else:
-            return False
+            return True
 
         url = json_result["next"]
 
@@ -51,8 +51,13 @@ def get_item_detail_data(endpoint, lookup="name"):
         json_result = result.json()
         print(json_result['name'])
 
-        if input("Press enter to keep going, "):
-            break
+        # if input("Press enter to keep going, "):
+        #     break
+
+        if not input("Press enter to search again, or type something to cancel "):
+            url = "http://pokeapi.co/api/v2/item/{}/".format(endpoint)
+        else:
+            return True
 
         url = json_result["next"]
 
@@ -70,8 +75,13 @@ def get_version_detail_data(endpoint, lookup="name"):
         json_result = result.json()
         print(json_result['name'])
 
-        if input("Press enter to keep going, "):
-            break
+        # if input("Press enter to keep going, "):
+        #     break
+
+        if not input("Press enter to search again, or type something to cancel "):
+            url = "http://pokeapi.co/api/v2/generation/{}/".format(endpoint)
+        else:
+            return True
 
         url = json_result["next"]
 #
@@ -86,14 +96,20 @@ def user_input():
         print("list")
         value = input("List of: pokemon, generation, item: ").lower()
         get_list_data(value)
+
     elif user_choice == "pokemon":
         print("pokemon")
         value = input("Type the id or name: ")
         get_pokemon_detail_data(value)
+
     elif user_choice == "item":
         print("item")
+        value = input("Type the id or name: ")
+        get_item_detail_data(value)
+
     elif user_choice == "generation":
         print("generation")
+
     else:
         print(user_choice)
         print("End Program")
